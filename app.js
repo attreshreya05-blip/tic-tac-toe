cb730c2d429077a0e7acdf5e2bb9123bb2112d20
attreshreya05-blip <attreshreya05@gmail.com>
2026-09-20 23:04:23 +0530

    my tic tac toe gane

---
+++ app.js
@@ -0,0 +1,56 @@
+let boxes = document.querySelectorAll(".box");
+
+let turn0 = true;
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+
+const winnerText = document.querySelector("#winner");
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+
+        if (turn0) {
+            box.innerText = "O";
+            turn0 = false;
+        } else {
+            box.innerText = "X";
+            turn0 = true;
+        }
+
+        box.disabled = true;
+
+        checkWinner();
+    });
+});
+
+const checkWinner = () => {
+
+    for (let pattern of winPatterns) {
+
+        let pos1 = boxes[pattern[0]].innerText;
+        let pos2 = boxes[pattern[1]].innerText;
+        let pos3 = boxes[pattern[2]].innerText;
+
+        if (pos1 !== "" && pos1 === pos2 && pos2 === pos3) {
+
+            winnerText.innerText = `Winner is ${pos1}!`;
+
+            boxes.forEach((box) => {
+                box.disabled = true;
+            });
+
+            return;
+        }
+    }
+};
+
+
